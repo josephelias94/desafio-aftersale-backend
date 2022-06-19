f159e57dcdd8ad09bdbd14f3ca6fb7274af995d3
josephelias94 <jose.elias.0703@gmail.com>
2022-06-19 16:11:57 -0300

    trello-07 - add reports folder to gitignore; - add pest and infection packges; - add shortcut to pest and infection; - change previous test to pest syntax.

--- tests/Feature/User/UserTest.php
+++ tests/Feature/User/UserTest.php
@@ -1,126 +1,97 @@
 <?php
 
-namespace Tests\Feature\User;
-
 use App\Models\User;
-use Spatie\Snapshots\MatchesSnapshots;
-use Tests\TestCase;
 
-class UserTest extends TestCase
-{
-    use MatchesSnapshots;
+use function Pest\Faker\faker;
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas, postJson};
+use function Spatie\Snapshots\assertMatchesJsonSnapshot;
 
-    /**
-     * @test
-     */
-    public function itShouldRegisterNewUser(): void
-    {
-        $this->assertDatabaseCount('users', 0);
+it('should register a new user', function () {
+    assertDatabaseCount('users', 0);
 
-        $body = [
-            'name' => 'my fullname',
-            'email' => 'email@email.com',
-            'password' => 'some-password',
-        ];
+    $body = [
+        'name' => faker()->name,
+        'email' => faker()->email,
+        'password' => 'some-password',
+    ];
 
-        $response = $this->postJson('/register', $body)
-            ->assertStatus(201);
+    $response = postJson('/register', $body)
+        ->assertStatus(201);
 
-        $this->assertMatchesJsonSnapshot($response->getContent());
+    assertMatchesJsonSnapshot($response->getContent());
+    assertDatabaseHas('users', [
+        'name' => $body['name'],
+        'email' => $body['email']
+    ]);
+});
 
-        $this->assertDatabaseHas('users', [
-            'name' => 'my fullname',
-            'email' => 'email@email.com'
-        ]);
-    }
+it('should not register a user with a repeated e-mail', function () {
+    $user = User::factory()->create(['email' => faker()->email]);
+    assertDatabaseCount('users', 1);
 
-    /**
-     * @test
-     */
-    public function itShouldNotRegisterUserWithSameEmail(): void
-    {
-        User::factory()->create(['email' => 'email@email.com']);
-        $this->assertDatabaseCount('users', 1);
+    $body = [
+        'name' => faker()->name,
+        'email' => $user->email,
+        'password' => 'some-password',
+    ];
 
-        $body = [
-            'name' => 'my fullname',
-            'email' => 'email@email.com',
-            'password' => 'some-password',
-        ];
+    $response = postJson('/register', $body)
+        ->assertStatus(422);
 
-        $response = $this->postJson('/register', $body)
-            ->assertStatus(422);
+    assertMatchesJsonSnapshot($response->getContent());
+    assertDatabaseCount('users', 1);
+});
 
-        $this->assertMatchesJsonSnapshot($response->getContent());
+it('should not register a user who fails at validation', function (
+    mixed $name,
+    mixed $email,
+    mixed $password
+) {
+    $body = [
+        'name' => $name,
+        'email' => $email,
+        'password' => $password,
+    ];
 
-        $this->assertDatabaseCount('users', 1);
-    }
+    $response = postJson('/register', $body)
+        ->assertStatus(422);
 
-    /**
-     * @test
-     * @dataProvider provideUsersWhoWillFailRegisterValidation
-     */
-    public function itShouldNotRegisterUserWhoFailsValidation(
-        mixed $name,
-        mixed $email,
-        mixed $password
-    ): void {
-
-        $body = [
-            'name' => $name,
-            'email' => $email,
-            'password' => $password,
-        ];
-
-        $response = $this->postJson('/register', $body)
-            ->assertStatus(422);
-
-        $this->assertMatchesJsonSnapshot($response->getContent());
-
-        $this->assertDatabaseCount('users', 0);
-    }
-
-    /**
-     * @mock
-     */
-    protected function provideUsersWhoWillFailRegisterValidation(): array
-    {
-        return [
-            'user with nullable name' => [
-                'name' => '',
-                'email' => 'email@email.com',
-                'password' => 'pass'
-            ],
-            'user with invalid name' => [
-                'name' => 123,
-                'email' => 'email@email.com',
-                'password' => 'pass'
-            ],
-            'user with nullable email' => [
-                'name' => 'my fullname',
-                'email' => '',
-                'password' => 'pass'
-            ],
-            'user with invalid email' => [
-                'name' => 'my fullname',
-                'email' => 'email.com',
-                'password' => 'pass'
-            ],
-            'user with nullable password' => [
-                'name' => 'my fullname',
-                'email' => 'email@email.com',
-                'password' => ''
-            ],
-            'user with invalid password' => [
-                'name' => 'my fullname',
-                'email' => 'email@email.com',
-                'password' => '123'
-            ],
-            'user with all invalid fields' => [
-                'name' => 123,
-                'email' => 'email.com',
-                'password' => '123'
-            ],
-        ];
-    }
-}
+    assertMatchesJsonSnapshot($response->getContent());
+    assertDatabaseCount('users', 0);
+})->with([
+    'user with no name' => [
+        'name' => null,
+        'email' => faker()->unique()->email,
+        'password' => faker()->lexify('?????'),
+    ],
+    'user with an invalid name' => [
+        'name' => faker()->randomDigit,
+        'email' => faker()->unique()->email,
+        'password' => faker()->lexify('?????'),
+    ],
+    'user with no email' => [
+        'name' => faker()->name,
+        'email' => null,
+        'password' => faker()->lexify('?????'),
+    ],
+    'user with an invalid email' => [
+        'name' => faker()->name,
+        'email' => faker()->randomDigit,
+        'password' => faker()->lexify('?????'),
+    ],
+    'user with no password' => [
+        'name' => faker()->name,
+        'email' => faker()->unique()->email,
+        'password' => null
+    ],
+    'user with an invalid password' => [
+        'name' => faker()->name,
+        'email' => faker()->unique()->email,
+        'password' => faker()->randomDigit,
+    ],
+    'user with all invalid fields' => [
+        'name' => faker()->randomDigit,
+        'email' => faker()->randomDigit,
+        'password' => faker()->randomDigit,
+    ],
+]);
